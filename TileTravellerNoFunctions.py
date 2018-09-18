cadc149bb0b4c1998b3d5085919e06e81df23503
sigurdurha320 <sigurdurhj320@nemi.tskoli.is>
2018-09-18 10:04:26 +0000

    minimizing redundency

--- TileTravellerNoFunctions.py
+++ TileTravellerNoFunctions.py
@@ -16,42 +16,83 @@ while cordX != 3 or cordY != 1:
     if (cordX == 1):
         if (cordY == 1):
             print("You can travel: (N)orth.")
-            possible="n"
+            direction = input("Direction: ").lower()
+            if direction == 'n' and cordY != 3:
+                cordY += 1
+            else:
+                print("Not a valid direction!")
+
         elif (cordY == 2):
             print("You can travel: (N)orth or (E)ast or (S)outh.")
-            possible="nes"
+            direction = input("Direction: ").lower()
+            if direction == 'n' and cordY != 3:
+                cordY += 1
+            elif direction == 's' and cordY != 1:
+                cordY -= 1
+            elif direction == 'e' and cordX != 3:
+                cordX += 1
+            else:
+                print("Not a valid direction!")
+
         elif (cordY == 3):
             print("You can travel: (E)ast or (S)outh.")
-            possible="es"
+            direction=input("Direction: ").lower()
+            if direction =='s' and cordY!=1:
+                cordY-=1
+            elif direction == 'e' and cordX != 3:
+                cordX+=1
+            else:
+                print("Not a valid direction!")
+
     if (cordX == 2):
         if (cordY == 1):
             print("You can travel: (N)orth.")
-            possible="n"
+            direction=input("Direction: ").lower()
+            if direction=='n' and cordY !=3:
+                cordY+=1
+            else:
+                print("Not a valid direction!")
         elif (cordY == 2):
             print("You can travel: (S)outh or (W)est.")
-            possible="sw"
+
+            direction = input("Direction: ").lower()
+            if direction == 's' and cordY != 1:
+                cordY -= 1
+            elif direction == 'w' and cordX != 1:
+                cordX -= 1
+            else:
+                print("Not a valid direction!")
+
         elif (cordY == 3):
             print("You can travel: (E)ast or (W)est.")
-            possible="ew"
+            direction = input("Direction: ").lower()
+            if direction == 'e' and cordX != 3:
+                cordX += 1
+            elif direction == 'w' and cordX != 1:
+                cordX -= 1
+            else:
+                print("Not a valid direction!")
     if (cordX == 3):
         if (cordY == 2):
             print("You can travel: (N)orth or (S)outh.")
-            possible="ns"
+
+            direction = input("Direction: ").lower()
+            if direction == 'n' and cordY != 3:
+                cordY += 1
+            elif direction == 's' and cordY != 1:
+                cordY -= 1
+            else:
+                print("Not a valid direction!")
         elif (cordY == 3):
             print("You can travel: (S)outh or (W)est")
-            possible="sw"
-    direction=input("Direction: ").lower()
-    if(direction in possible) and len(direction)==1:
-        if direction=='n' and cordY !=3:
-            cordY+=1
-        elif direction =='s' and cordY!=1:
-            cordY-=1
-        elif direction == 'e' and cordX != 3:
-            cordX+=1
-        elif direction == 'w' and cordX != 1:
-            cordX-=1
-        else:
-            print("Not a valid direction!",cordX,cordY)
-    else:
-        print("Not a valid direction!",cordX,cordY)
+            direction = input("Direction: ").lower()
+            if direction == 's' and cordY != 1:
+                cordY -= 1
+
+            elif direction == 'w' and cordX != 1:
+                cordX -= 1
+            else:
+                print("Not a valid direction!")
+
+
 print("Victory!")
